bc27fc40269d26569da0261edb1bf5589d9b157d
pierre-alexandre mahé <pierre-alexandre.mahe@soprasteria.com>
2020-06-09 16:06:57 +0200

    commit cdap-build

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,5 @@
 export MAVEN_OPTS="-Xmx6144m"
 mvn clean install -P spark1-dev,spark2-dev,templates,dist,release,deb-prepare,deb,tgz \
  -Drat.skip -Dcheckstyle.skip -Dmaven.javadoc.skip -Dmaven.source.skip -Dmaven.test.skip -Dgpg.skip \
- -Dadditional.artifacts.dir=$(pwd)/app-artifacts \
  -DbuildNumber=42 \
  $*
